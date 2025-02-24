f42bd9eadba59b057135ddf74d9c9503f128f904
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-02-24 23:47:09 +0530

    check if the given no. is a palindrome

---
+++ basics/palindromeNumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, rev = 0, temp;
+    cout << "Enter a number: ";
+    cin >> num;
+    temp = num;
+
+    while (temp > 0) {
+        rev = rev * 10 + temp % 10;
+        temp /= 10;
+    }
+
+    if (num == rev)
+        cout << "Palindrome Number" << endl;
+    else
+        cout << "Not a Palindrome" << endl;
+
+    return 0;
+}
