71f6472ecfe7aae3a98152aa85671f7bcaa7cbae
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-03-08 23:59:13 +0530

    Gcd of the given no.

--- basics/VowelsCounting.cpp
+++ basics/VowelsCounting.cpp
@@ -32,7 +32,7 @@ int main() {
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
     
-    cout << "GCD of " << num1 << " and " << num2 << " is: " << gcd(num1, num2) << endl;
+    cout << "GCD of the given number is  " << num1 << " and " << num2 << " is: " << gcd(num1, num2) << endl;
     
     string input;
     cout << "Enter a string: ";
