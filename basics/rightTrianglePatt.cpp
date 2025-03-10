8560c7e4443501c89fba5c318260b37777259f3b
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-03-10 23:54:13 +0530

    right angle triangle pattern with chracters

---
+++ basics/rightTrianglePatt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 3;  
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << char('A' + i);
+        }
+        cout << "\n";
+    }
+    return 0;
+}
