34a6f632acfc9eccc438eb386c92952aa7cafba3
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-03-10 23:57:41 +0530

    print a pyramid pattern with increasing star

---
+++ basics/pyramidPattIncreasingStar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 3;  
+    for (int i = 0; i < n; i++) {
+        // Print spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+        // Print stars
+        for (int j = 0; j < 2 * i + 1; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
