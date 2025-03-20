435cc6d4dc368d4238e14e13d1e2382167d9285c
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-03-20 23:59:23 +0530

    Pattern with Rows of Increasing Characters

---
+++ basics/PattRows.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void printPattern(int n) {
+    char ch = 'A'; // Start from 'A'
+    for (int i = 1; i <= n; i++) { // Loop for rows
+        for (int j = 1; j <= i; j++) { // Loop for columns
+            cout << ch; // Print the character
+            ch++; // Increment character
+        }
+        cout << endl; // Move to the next line
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    printPattern(n);
+    return 0;
+}
