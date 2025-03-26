47218684de94aae59fd7e0e5da6fb3d7200d50d6
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-03-26 23:54:45 +0530

    alternating rows and coloumn

---
+++ Array/AlternatingRowsCol.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void printAlternatingPattern(int n) {
+    int num = 1; // Starting number
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << num++;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    printAlternatingPattern(n);
+    return 0;
+}
