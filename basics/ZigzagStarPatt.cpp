61b1939e26289a2e1d0a9e021c370902024091f9
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-03-27 23:55:55 +0530

    function change

--- basics/ZigzagStarPatt.cpp
+++ basics/ZigzagStarPatt.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void printZigzagPattern(int n) {
+void printZigzagPatt(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             // Print star for even rows or shifted stars for odd rows
@@ -21,6 +21,6 @@ int main() {
     int n;
     cout << "Enter the value of n: ";
     cin >> n;
-    printZigzagPattern(n);
+    printZigzagPatt(n);
     return 0;
 }
