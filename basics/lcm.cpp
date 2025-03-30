8fb91aa677a2f01d504609a53e3534154f35f403
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-03-30 23:55:43 +0530

    Finding LCM

---
+++ basics/lcm.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate the Greatest Common Divisor (GCD) using Euclidean algorithm
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// Function to calculate the Least Common Multiple (LCM)
+int lcm(int a, int b) {
+    if (a == 0 || b == 0) {
+        return 0; // LCM is undefined for zero
+    }
+    return (a / gcd(a, b)) * b; // Using the formula: LCM(a,b) = (a*b)/GCD(a,b)
+}
+
+int main() {
+    int num1, num2;
+    
