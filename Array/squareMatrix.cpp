5b2a010c7381a7b7ab9c8ed86713ea5635e16db9
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-02 23:52:49 +0530

    fixed matrix[size][size]

--- Array/squareMatrix.cpp
+++ Array/squareMatrix.cpp
@@ -2,29 +2,38 @@
 using namespace std;
 
 void generateMatrix(int size) {
-    int matrix[size][size];
+    int** matrix = new int*[size];
+    for (int i = 0; i < size; i++) {
+        matrix[i] = new int[size];
+    }
+
     int num = 1;
-    
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             matrix[i][j] = num++;
         }
     }
-    
+
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
     }
+
+    // Free allocated memory
+    for (int i = 0; i < size; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
 }
 
 int main() {
     int size;
     cout << "Enter the size of the square matrix: ";
     cin >> size;
-    
+
     generateMatrix(size);
-    
+
     return 0;
 }
