9d91a83e9f44be8eae264544177ce93c6185f00b
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-06 23:50:42 +0530

    two sum

---
+++ Array/twoSum.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void twoSum(int nums[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (nums[i] + nums[j] == target) {
+                cout << "Indices: [" << i << ", " << j << "]" << endl;
+                cout << "Values: " << nums[i] << " + " << nums[j] << " = " << target << endl;
+                return;
+            }
+        }
+    }
+    cout << "No two numbers add up to the target." << endl;
+}
+
+int main() {
+    // Test case 1
+    int nums1[] = {2, 7, 11, 15};
+    int size1 = sizeof(nums1)/sizeof(nums1[0]);
+    int target1 = 9;
+    twoSum(nums1, size1, target1);
+
+    // Test case 2
+    int nums2[] = {3, 2, 4};
+    int size2 = sizeof(nums2)/sizeof(nums2[0]);
+    int target2 = 6;
+    twoSum(nums2, size2, target2);
+
+    // Test case 3
+    int nums3[] = {3, 3};
+    int size3 = sizeof(nums3)/sizeof(nums3[0]);
+    int target3 = 6;
+    twoSum(nums3, size3, target3);
+
+    return 0;
+}
