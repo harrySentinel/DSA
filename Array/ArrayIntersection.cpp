2b1b72e03c1482f7ba5039f8d120c432c6698beb
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-07 23:52:07 +0530

    used vector for array intersection

--- Array/ArrayIntersection.cpp
+++ Array/ArrayIntersection.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-// Function to find intersection of two arrays
-void findIntersection(int arr1[], int size1, int arr2[], int size2) {
-    // Create a copy of the second array
-    int arr2Copy[size2];
-    for (int i = 0; i < size2; i++) {
-        arr2Copy[i] = arr2[i];
-    }
+vector<int> findIntersection(const vector<int>& nums1, const vector<int>& nums2) {
+    vector<int> result;
+    vector<bool> used(nums2.size(), false); // Track used elements
 
-    cout << "Intersection: ";
-    
-    // Check each element of first array
-    for (int i = 0; i < size1; i++) {
-        // Compare with each element of second array's copy
-        for (int j = 0; j < size2; j++) {
-            if (arr1[i] == arr2Copy[j]) {
-                cout << arr1[i] << " ";
-                // Mark as found by setting to a dummy value
-                arr2Copy[j] = INT_MIN; 
-                break;
+    for (int i = 0; i < nums1.size(); i++) {
+        for (int j = 0; j < nums2.size(); j++) {
+            if (nums1[i] == nums2[j] && !used[j]) {
+                result.push_back(nums1[i]);
+                used[j] = true;
+                break; // Move to next nums1 element
             }
         }
     }
-    cout << endl;
+
+    return result;
 }
 
 int main() {
-    // Example arrays
-    int arr1[] = {3, 5, 2, 2, 8};
-    int arr2[] = {2, 9, 5, 2, 1};
-    
-    // Calculate sizes
-    int size1 = sizeof(arr1)/sizeof(arr1[0]);
-    int size2 = sizeof(arr2)/sizeof(arr2[0]);
-    
-    // Find and print intersection
-    findIntersection(arr1, size1, arr2, size2);
-    
+    vector<int> arr1;
+    arr1.push_back(3);
+    arr1.push_back(5);
+    arr1.push_back(2);
+    arr1.push_back(2);
+    arr1.push_back(8);
+
+    vector<int> arr2;
+    arr2.push_back(2);
+    arr2.push_back(9);
+    arr2.push_back(5);
+    arr2.push_back(2);
+    arr2.push_back(1);
+
+    vector<int> intersection = findIntersection(arr1, arr2);
+
+    cout << "Intersection: ";
+    for (int i = 0; i < intersection.size(); i++) {
+        cout << intersection[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
