ca0b3c230dabcf43370cc9db5861f898fec82d30
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-10 23:59:47 +0530

    adding alternate mothod of occurence of digits

--- basics/occurenceofDigits.cpp
+++ basics/occurenceofDigits.cpp
@@ -25,3 +25,41 @@ int main() {
 
     return 0;
 }
+
+// alternate method 
+
+// #include <iostream>
+// using namespace std;
+
+// int countDigitOccurrences(int number, int digit) {
+//     int count = 0;
+
+//     // Handle negative numbers
+//     if (number < 0) number = -number;
+
+//     while (number > 0) {
+//         int currentDigit = number % 10;
+//         if (currentDigit == digit) {
+//             count++;
+//         }
+//         number /= 10;
+//     }
+
+//     return count;
+// }
+
+// int main() {
+//     int number, digit;
+
+//     cout << "Enter a number: ";
+//     cin >> number;
+
+//     cout << "Enter the digit to count: ";
+//     cin >> digit;
+
+//     int result = countDigitOccurrences(number, digit);
+
+//     cout << "The digit " << digit << " occurs " << result << " times in " << number << "." << endl;
+
+//     return 0;
+// }
