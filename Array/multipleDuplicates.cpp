2b8a53e15b1b2b72aaae877b72234549462785c0
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-15 23:58:54 +0530

    added simple for loop in comments

--- Array/multipleDuplicates.cpp
+++ Array/multipleDuplicates.cpp
@@ -2,21 +2,35 @@
 #include <vector>
 using namespace std;
 
-vector<int> findDuplicates(const vector<int>& arr) {
+vector<int> findDuplicates(const vector<int> &arr)
+{
     int maxVal = 0;
 
     // Find the maximum value in the array
-    for (int num : arr) {
-        if (num > maxVal) maxVal = num;
+    for (int num : arr)
+    {
+        if (num > maxVal)
+            maxVal = num;
     }
+
+    // int maxVal = arr[0]; // Start with the first element
+    // for (int i = 1; i < arr.size(); i++)
+    // {
+    //     if (arr[i] > maxVal)
+    //     {
+    //         maxVal = arr[i];
+    //     }
+    // }
 
     // Frequency array
     vector<int> freq(maxVal + 1, 0);
     vector<int> duplicates;
 
-    for (int num : arr) {
+    for (int num : arr)
+    {
         freq[num]++;
-        if (freq[num] == 2) {
+        if (freq[num] == 2)
+        {
             duplicates.push_back(num);
         }
     }
@@ -24,12 +38,14 @@ vector<int> findDuplicates(const vector<int>& arr) {
     return duplicates;
 }
 
-int main() {
+int main()
+{
     vector<int> arr = {1, 2, 3, 4, 2, 3, 5, 6, 3, 2};
     vector<int> result = findDuplicates(arr);
 
     cout << "Duplicate numbers: ";
-    for (int num : result) {
+    for (int num : result)
+    {
         cout << num << " ";
     }
     cout << endl;
