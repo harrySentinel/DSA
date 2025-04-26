94835eb363a7ada11e0f60618a16bcf5a153448e
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-04-26 23:58:44 +0530

    another logic to solve gcd of a number

--- basics/GCD.cpp
+++ basics/GCD.cpp
@@ -19,3 +19,20 @@ int main() {
     cout << "GCD of the givn number is " << num1 << " and " << num2 << " is: " << gcd(num1, num2) << endl;
     return 0;
 }
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n1 = 18, n2 = 45, gcd = 1;
+    
+//     for(int i = 1; i <= n1 || i <= n2; i++) {
+//         if(n1 % i == 0 && n2 % i == 0)
+//             gcd = i;
+//     }
+    
+//     cout<<"The GCD is "<< gcd;
+    
+//     return 0;
+// }
