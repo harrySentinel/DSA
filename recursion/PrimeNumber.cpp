d2022ba8ac6321d25ecce931c17c19a313c86990
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-05-03 23:55:28 +0530

    added method 2 other than recursion

--- recursion/PrimeNumber.cpp
+++ recursion/PrimeNumber.cpp
@@ -21,3 +21,30 @@ int main()
 
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+   for(int i=2; i<=sqrt(n); i++){
+       if(n%i==0)
+         return false;
+   }
+
+   return true;
+}
+
+
+int main()
+{
+   int n = 21;
+   if (isPrime(n))
+      cout << "Prime Number";
+   else
+      cout << "Not Prime";
+
+   return 0;
+}
+*/
