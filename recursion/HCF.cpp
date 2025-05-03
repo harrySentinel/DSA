8cb361dff8f1265a1ab5a5d2c3346fc18dfa57ad
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2025-05-03 23:58:20 +0530

    method 2 of finding HCF

--- recursion/HCF.cpp
+++ recursion/HCF.cpp
@@ -30,3 +30,24 @@ int main()
 
    return 0;
 }
+
+/*
+#include<iostream>
+using namespace std;
+
+int getHCF(int a, int b)
+{
+  return b == 0 ? a : getHCF(b, a % b);
+}
+
+int main()
+{
+  int num1 = 96, num2 = 56, HCF;
+
+  HCF = getHCF(num1, num2);
+
+  cout<<"HCF of "<<num1<<" and "<<num2<<" is "<<HCF;
+
+  return 0;
+}
+*/
