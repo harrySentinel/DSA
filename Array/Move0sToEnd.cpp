6a8f38965c9cfb760b8c75b13b5533e2c83606dd
Aditya Srivastava <adityasri1801@gmail.com>
2025-05-21 21:49:36 +0530

    move all zeros to end using two traversals

--- Array/Move0sToEnd.cpp
+++ Array/Move0sToEnd.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 using namespace std;
 
+//  move all zeros to end using temporary array
+
 // function to move all zeros to the end
 void pushZerosToEnd(vector<int> &arr) {
     int n = arr.size();
@@ -36,3 +38,38 @@ int main() {
     }
     return 0;
 }
+
+
+//move all zeros to end using two traversals
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// // Function which pushes all zeros to end 
+// void pushZerosToEnd(vector<int>& arr) {
+    
+//     // Count of non-zero elements
+//     int count = 0;  
+
+//     // If the element is non-zero, replace the element at
+//     // index 'count' with this element and increment count
+//     for (int i = 0; i < arr.size(); i++) {
+//         if (arr[i] != 0)
+//             arr[count++] = arr[i];
+//     }
+
+//     // Now all non-zero elements have been shifted to
+//     // the front. Make all elements 0 from count to end.
+//     while (count < arr.size())
+//         arr[count++] = 0;
+// }
+
+// int main() {
+//     vector<int> arr = {1, 2, 0, 4, 3, 0, 5, 0};
+//     pushZerosToEnd(arr);
+//     for (int num : arr) {
+//         cout << num << " ";
+//     }
+//     return 0;
+// }
