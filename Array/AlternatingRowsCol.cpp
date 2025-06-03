1129cb1e0a26a528c864f9b90ef4d657d9044823
Aditya Srivastava <adityasri1801@gmail.com>
2025-06-03 17:31:52 +0530

    remove starting number

--- Array/AlternatingRowsCol.cpp
+++ Array/AlternatingRowsCol.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void printAlternatingPattern(int n) {
-    int num = 1; // Starting number
+    int num = 1; 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cout << num++;
